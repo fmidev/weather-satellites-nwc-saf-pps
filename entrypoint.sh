f3fb3c7abf468fcfc3d2220fb4612e7fc7a3cdba
Panu Lahtinen <panu.lahtinen@fmi.fi>
2024-10-03 14:42:32 +0300

    Read env variables after activating mamba env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 source /opt/conda/.bashrc
-source /config/env-variables
 
 micromamba activate
 
-/usr/bin/run_pps.py -c /config/run_pps.yaml
+source /config/env-variables
 
+/usr/bin/run_pps.py /config/run_pps.yaml
